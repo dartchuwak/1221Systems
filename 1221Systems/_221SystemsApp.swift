a4329eb41e490e2af7149bddb93d7c86a648bafd
Evgenii Mikhailov <e.mikhailov@outlook.com>
2024-01-31 16:54:03 +0300

    Initial Commit

---
+++ 1221Systems/_221SystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _221SystemsApp.swift
+//  1221Systems
+//
+//  Created by Evgenii Mikhailov on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct _221SystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
